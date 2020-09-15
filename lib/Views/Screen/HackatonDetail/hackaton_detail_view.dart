7c7c39d6192334a3c30d93f334c4b088add7a420
Furkan Sevgili <fs.2838@gmail.com>
2020-09-16 00:38:14 +0300

    added hackaton detail view

---
+++ lib/Views/Screen/HackatonDetail/hackaton_detail_view.dart
@@ -0,0 +1,105 @@
+import 'dart:ui' show Radius;
+
+import 'package:FindHackathon/Core/Extension/context_extension.dart';
+import 'package:FindHackathon/Views/Screen/HackatonDetail/hackaton_detail_view_model.dart';
+import 'package:FindHackathon/Views/Widgets/fat_button.dart';
+import 'package:flutter/material.dart';
+
+class HackatonDetail extends StatefulWidget {
+  HackatonDetail({Key key}) : super(key: key);
+
+  @override
+  _HackatonDetailState createState() => _HackatonDetailState();
+}
+
+class _HackatonDetailState extends State<HackatonDetail> {
+  HackatonDetailModel detailModel = new HackatonDetailModel();
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    detailModel = getHackatonDetail();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+          //TODO:BottomNavigationBar
+          body: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(context.constMediumValue),
+                    bottomRight: Radius.circular(context.constMediumValue)),
+                child: Container(
+                    child: PageView.builder(
+                  itemBuilder: (context, index) {
+                    return Image.asset(
+                      detailModel.imagePaths[index],
+                      fit: BoxFit.cover,
+                    );
+                  },
+                  itemCount: detailModel.imagePaths.length,
+                ))),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: context.constLowValue),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                detailModel.name,
+                style: context.textTheme.headline5,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: context.constLowValue),
+              child: Row(
+                children: [
+                  Icon(Icons.star,
+                      color:
+                          detailModel.stars >= 1 ? Colors.yellow : Colors.grey),
+                  Icon(Icons.star,
+                      color:
+                          detailModel.stars >= 2 ? Colors.yellow : Colors.grey),
+                  Icon(Icons.star,
+                      color:
+                          detailModel.stars >= 3 ? Colors.yellow : Colors.grey),
+                  Icon(Icons.star,
+                      color:
+                          detailModel.stars >= 4 ? Colors.yellow : Colors.grey),
+                  Icon(Icons.star,
+                      color:
+                          detailModel.stars >= 5 ? Colors.yellow : Colors.grey)
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: context.constLowValue),
+            child: Text(
+              detailModel.description,
+              style: context.textTheme.bodyText2,
+            ),
+          ),
+          Spacer(
+            flex: 1,
+          ),
+          FatButton(
+            text: "Join",
+          ),
+          Spacer(
+            flex: 1,
+          ),
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/Views/Screen/HackatonDetail/hackaton_detail_view_model.dart
@@ -0,0 +1,58 @@
+import 'package:FindHackathon/Core/Base/base_view_model.dart';
+
+class HackatonDetailModel extends BaseViewModel {
+  String name;
+  String description;
+  int stars;
+  List<String> imagePaths;
+//Todo:User,Profile Pictures
+  void setName(String getName) {
+    name = getName;
+  }
+
+  void setDescription(String getDescription) {
+    description = getDescription;
+  }
+
+  void setStars(int getStars) {
+    stars = getStars;
+  }
+
+  void addImagePath(String getImagePath) {
+    imagePaths.add(getImagePath);
+  }
+
+  String getName() {
+    return name;
+  }
+
+  String getDescription() {
+    return description;
+  }
+
+  List<String> getImagePaths() {
+    return imagePaths;
+  }
+
+  int getStars() {
+    return stars;
+  }
+
+  @override
+  void init() {
+    // TODO: implement init
+  }
+}
+
+HackatonDetailModel getHackatonDetail() {
+  HackatonDetailModel model = new HackatonDetailModel();
+  model.imagePaths = new List<String>();
+  model.setName("VBT Hackaton");
+  model.setDescription(
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'");
+  model.imagePaths.add('assets/images/illustration.png');
+  model.imagePaths.add('assets/images/illustration.png');
+  model.imagePaths.add('assets/images/illustration.png');
+  model.setStars(4);
+  return model;
+}
