c7ddcfa2bcb9e36955a7acd9e2c38f89f6a975e8
Furkan Sevgili <fs.2838@gmail.com>
2020-09-15 11:06:49 +0300

    added hackaton_details_view_model

--- lib/Views/Screen/HackatonDetail/hackaton_detail_view_model.dart
+++ lib/Views/Screen/HackatonDetail/hackaton_detail_view_model.dart
@@ -1,6 +1,34 @@
 import 'package:FindHackathon/Core/Base/base_view_model.dart';
 
 class HackatonDetaildViewModel extends BaseViewModel {
+  String name;
+  String description;
+  int stars;
+
+  void setName(String getName) {
+    name = getName;
+  }
+
+  void setDescription(String getDescription) {
+    description = getDescription;
+  }
+
+  void setStars(int getStars) {
+    stars = getStars;
+  }
+
+  String getName() {
+    return name;
+  }
+
+  String getDescription() {
+    return description;
+  }
+
+  int getStars() {
+    return stars;
+  }
+
   @override
   void init() {
     // TODO: implement init
