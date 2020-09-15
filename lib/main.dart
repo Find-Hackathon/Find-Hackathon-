ed22367a337b1d9feff046c85962faa9416f0de6
Furkan Sevgili <fs.2838@gmail.com>
2020-09-15 16:34:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:FindHackathon/Views/Screen/HackatonDetail/hackaton_detail_view.dart';
-import 'package:FindHackathon/Views/Screen/Splash/splash_view.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,8 +7,11 @@ import 'Core/Notifier/app_provider.dart';
 import 'Core/Notifier/theme_provider.dart';
 import 'Core/Service/Localization/language_service.dart';
 import 'Core/Service/Navigation/navigation_service.dart';
+import 'Views/Screen/Splash/splash_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     EasyLocalization(
         path: LanguageService.path,
@@ -25,12 +27,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
       navigatorKey: NavigatorService.instance.navigatorKey,
       theme: Provider.of<ThemeProvider>(context).getTheme,
-      home: HackatonDetail(),
+      home: SplashScreen(),
     );
   }
 }
