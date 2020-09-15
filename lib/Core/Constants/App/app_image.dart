226e7ed6a7d129b07b438494eec9a07964bec89c
Fatih Kurçenli <rizelifatih5353@gmail.com>
2020-09-15 13:06:10 +0300

    welcome screen page added

--- lib/Core/Constants/App/app_image.dart
+++ lib/Core/Constants/App/app_image.dart
@@ -2,4 +2,5 @@ class AppImage {
   static const imagesPath = "assets/images/";
 
   static const splashLogo = "${imagesPath}splash_logo.png";
+  static const welcomeImage = "${imagesPath}welcome.png";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:FindHackathon/Views/Screen/Splash/splash_view.dart';
+import 'Views/Screen/Splash/splash_view.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
