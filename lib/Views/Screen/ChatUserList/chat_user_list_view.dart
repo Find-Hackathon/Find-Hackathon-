895986a82b2e61ea53317545ed3bd03535584929
Furkan Sevgili <fs.2838@gmail.com>
2020-09-16 12:46:55 +0300

    added listview to chat users

--- lib/Views/Screen/ChatUserList/chat_user_list_view.dart
+++ lib/Views/Screen/ChatUserList/chat_user_list_view.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
+import 'chat_user_list_view_model.dart';
+
+class ChatUserListView extends StatefulWidget {
+  ChatUserListView({Key key}) : super(key: key);
 
   @override
   _ChatUserListState createState() => _ChatUserListState();
 }
+
+class _ChatUserListState extends State<ChatUserListView> {
+  List<ChatUserModel> users;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    users = mockUsers();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +31,20 @@ import 'package:flutter/widgets.dart';
       ),
     );
 =======
+        child: Scaffold(
+      body: new ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (BuildContext context, int index) {
+          return new Card(
+              child: ListTile(
+            leading: CircleAvatar(
+              child: Image.asset(users[index].getUserProfilePicture()),
+            ),
+            title: Text(users[index].getUserName()),
+          ));
+        },
+      ),
+    ));
 >>>>>>> Stashed changes
   }
 }
