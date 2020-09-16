c9db7f30263c721550f2a66934b5b4318446cf5d
Furkan Sevgili <fs.2838@gmail.com>
2020-09-16 12:46:47 +0300

    added listview to chat users

--- lib/Views/Screen/ChatUserList/chat_user_list_view.dart
+++ lib/Views/Screen/ChatUserList/chat_user_list_view.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class ChatUserList extends StatefulWidget {
-  ChatUserList({Key key}) : super(key: key);
 
   @override
   _ChatUserListState createState() => _ChatUserListState();
 }
 
-class _ChatUserListState extends State<ChatUserList> {
   @override
   Widget build(BuildContext context) {
     return Container(
+<<<<<<< Updated upstream
       child: Scaffold(
         body: Container(
           child: Column(),
         ),
       ),
     );
+=======
+>>>>>>> Stashed changes
   }
 }
