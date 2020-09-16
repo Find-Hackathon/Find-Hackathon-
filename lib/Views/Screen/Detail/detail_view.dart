93ed86136e749ff5c7ebf417f51057a4563c93f9
Muhammed Burak Çakır <muhburcak@gmail.com>
2020-09-16 10:55:34 +0300

    initialized firebasechat

--- lib/Views/Screen/Detail/detail_view.dart
+++ lib/Views/Screen/Detail/detail_view.dart
@@ -1,6 +1,10 @@
+import 'package:FindHackathon/Views/Screen/Chat/chat_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../User.dart';
 
 class DetailView extends StatefulWidget {
   @override
@@ -8,41 +12,91 @@ class DetailView extends StatefulWidget {
 }
 
 class _DetailViewState extends State<DetailView> {
-  bool state;
-  SharedPreferences prefs;
-
   @override
   void initState() {
-    // _getUserState().then((state) {
-    //   setState(() {
-    //     this.state = state;
-    //   });
-    // });
     super.initState();
+
+    FirebaseFirestore.instance.collection('chat').snapshots().listen(
+        (event) => event.docs.forEach((element) => print(element.get('name'))));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('GRUP2'),
-        ),
-        body: Column(
-          children: [
-            // Text("state : " + prefs.getBool("isLoggedIn").toString()),
-            // Text("password : " + prefs.getString("password")),
-            // Text("email : " + prefs.get("email")),
-            OutlineButton(
-                child: Text("Logout"),
-                onPressed: () {
-                  // prefs.setBool("isLoggedIn", false);
-                  Navigator.pop(context);
-                },
-                shape: new RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(10.0)))
-          ],
-        ));
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('conversations')
+        //.where('members', arrayContains: User.userId)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text('Loading...');
+          }
+
+
+          return ListView(
+            children: snapshot.data.docs
+                .map((doc) =>
+                ListTile(
+                  // leading: CircleAvatar(backgroundImage: NetworkImage(doc.get("image"))),
+                  title: Text('title'),
+                  subtitle: Text(doc.get('displayMessage')),
+                  // trailing: Text(doc.get("time")),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) =>
+                          ChatPage(
+                            userId: User.userId,
+                            conversationId: doc.documentID,
+                          )),
+                    );
+                  },
+                ))
+                .toList(),
+          );
+        },
+      ),
+    );
   }
+
+
+  Future<DocumentSnapshot> getData() async {
+    await Firebase.initializeApp();
+    return await FirebaseFirestore.instance
+        .collection("chat")
+        .doc("Mdnxurl5UxXg9IUPAjFi")
+        .get();
+  }
+
+// return Container(
+//     child: FutureBuilder(
+//   future: getData(),
+//   builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+//     if (snapshot.connectionState == ConnectionState.done) {
+//       return Column(
+//         children: [
+//           Container(
+//             height: 27,
+//             child: Text(
+//               "Name: ${snapshot.data.data()['name']}",
+//               overflow: TextOverflow.fade,
+//               style: TextStyle(fontSize: 20),
+//             ),
+//           ),
+//         ],
+//       );
+//     } else if (snapshot.connectionState == ConnectionState.none) {
+//       return Text("No data");
+//     }
+//     return CircularProgressIndicator();
+//   },
+// )
+// );
 
 // _getUserState() async {
 //   prefs = await SharedPreferences.getInstance();
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:FindHackathon/Views/Home/home_view.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
