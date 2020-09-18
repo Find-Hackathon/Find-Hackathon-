f373b4d386384826ee91d76b839eebd9a554618d
Furkan Sevgili <fs.2838@gmail.com>
2020-09-18 03:59:25 +0300

    Detail http bişiler işte uykum geldi aklıma yazıcak bişi gelmiyo artık

--- lib/Views/Screen/HackatonDetail/hackaton_detail_view_model.dart
+++ lib/Views/Screen/HackatonDetail/hackaton_detail_view_model.dart
@@ -1,10 +1,19 @@
-import 'package:FindHackathon/Core/Base/base_view_model.dart';
+import 'dart:convert';
+import 'dart:io';
 
-class HackatonDetailModel extends BaseViewModel {
+import 'package:FindHackathon/Core/Base/base_view_model.dart';
+import 'package:http/http.dart ' as http;
+
+class HackatonDetailViewModel extends BaseViewModel {
   String name;
   String description;
   int stars;
-  List<String> imagePaths;
+  //List<String> imagePaths;
+  String imagePath;
+  bool isSubscribed;
+
+  String organizationUrl =
+      "https://find-hackathon.herokuapp.com/organizations/id/";
 //Todo:User,Profile Pictures
   void setName(String getName) {
     name = getName;
@@ -14,12 +23,14 @@ class HackatonDetailModel extends BaseViewModel {
     description = getDescription;
   }
 
+/*
   void setStars(int getStars) {
     stars = getStars;
   }
-
-  void addImagePath(String getImagePath) {
-    imagePaths.add(getImagePath);
+*/
+  void setImagePath(String getImagePath) {
+    //imagePaths.add(getImagePath);
+    imagePath = getImagePath;
   }
 
   String getName() {
@@ -30,6 +41,11 @@ class HackatonDetailModel extends BaseViewModel {
     return description;
   }
 
+  String getImagePath() {
+    return imagePath;
+  }
+
+/*
   List<String> getImagePaths() {
     return imagePaths;
   }
@@ -37,6 +53,7 @@ class HackatonDetailModel extends BaseViewModel {
   int getStars() {
     return stars;
   }
+*/
 
   @override
   void init() {
@@ -44,15 +61,37 @@ class HackatonDetailModel extends BaseViewModel {
   }
 }
 
-HackatonDetailModel getHackatonDetail() {
-  HackatonDetailModel model = new HackatonDetailModel();
-  model.imagePaths = new List<String>();
-  model.setName("VBT Hackaton");
-  model.setDescription(
-      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'");
-  model.imagePaths.add('assets/images/illustration.png');
-  model.imagePaths.add('assets/images/illustration.png');
-  model.imagePaths.add('assets/images/illustration.png');
-  model.setStars(4);
-  return model;
+class Subscription {
+  String subscribe;
+
+  Subscription({this.subscribe});
+
+  Subscription.fromJson(Map<String, dynamic> json) {
+    subscribe = json['subscribe'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['subscribe'] = this.subscribe;
+    return data;
+  }
+}
+
+Future<void> subscribeToOrganization(
+    String userId, String organizationId) async {
+  String subscribeUrl =
+      "https://find-hackathon.herokuapp.com/organizations/organizaionSubscribe/";
+  var subscription = new Subscription(subscribe: userId);
+  final http.Response response = await http.post(subscribeUrl + organizationId,
+      body: subscription.toJson());
+  switch (response.statusCode) {
+    case HttpStatus.ok:
+      var decodedJson = json.decode(response.body);
+      if (decodedJson["isSucces"]) {
+        print(decodedJson);
+      }
+      break;
+    default:
+      print("Something went wrong");
+  }
 }
